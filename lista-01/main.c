053eb4b45563550c1d50bb1afd01d47f9622b5a4
Victor Guerra <victor.eg@aluno.ifsc.edu.br>
2023-08-08 15:15:16 -0300

    Começo

--- lista-01/main.c
+++ lista-01/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[])
+{
+    int seleção;
+   printf("Bem-vindo(a) ao sistema\n");
+    printf("Selecione qual opção de exercício você irá utilizar:\n 1-Ler N números e mostrar o maior entre eles\n "
+           "2- Advinhador de números\n 3- Números pares, ímpares positivos e negativos\n4-Jogo de azar\n5-vetor\n6-Matriz Identidade\n"
+           "7-Palindromos\n8-Repetição de vogais");
+    scanf("%d",&seleção);
+
+
+
+
     return 0;
 }
