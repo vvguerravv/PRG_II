a7f4e2aa99b25cba8d1af60fe678bdb992274f9f
Victor Guerra <victor.eg@aluno.ifsc.edu.br>
2023-08-08 17:22:25 -0300

    Parte 1

--- lista-01/main.c
+++ lista-01/main.c
@@ -2,15 +2,37 @@
 
 int main(int argc, char *argv[])
 {
-    int seleção;
-   printf("Bem-vindo(a) ao sistema\n");
-    printf("Selecione qual opção de exercício você irá utilizar:\n 1-Ler N números e mostrar o maior entre eles\n "
-           "2- Advinhador de números\n 3- Números pares, ímpares positivos e negativos\n4-Jogo de azar\n5-vetor\n6-Matriz Identidade\n"
-           "7-Palindromos\n8-Repetição de vogais");
-    scanf("%d",&seleção);
+    int i,escolha, num, maior = 0;
+    printf("Bem-vindo(a) ao sistema\n");
+    printf("Tipos de exercícios:\n1-Ler N números e mostrar o maior entre eles\n"
+           "2- Advinhador de números\n3- Números pares, ímpares positivos e negativos\n4-Jogo de azar\n5-vetor\n6-Matriz Identidade\n"
+           "7-Palindromos\n8-Repetição de vogais\n\n");
+    printf("Selecione qual opção de exercício você irá utilizar: ");
+    scanf("%d",&escolha);
 
+    printf("\n");
 
+    switch (escolha)
+    {
+        case 1:
+            printf("Entre com os números desejados: ");
+            for (;num != 0;){
+                scanf("%d",&num);
+                if(num>maior){
+                    maior = num;
+                }
+            }
+            printf("O maior número é o %d\n", maior);
+            break;
 
+        case 2: printf("Não implementado\n"); break;
+        case 3: printf("Não implementado\n"); break;
+        case 4: printf("Não implementado\n"); break;
+        case 5: printf("Não implementado\n"); break;
+        case 6: printf("Não implementado\n"); break;
+        case 7: printf("Não implementado\n"); break;
+        case 8: printf("Não implementado\n"); break;
+    }
 
     return 0;
 }
