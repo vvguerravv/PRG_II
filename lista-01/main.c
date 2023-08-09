2317c016af25ca376ecb271107dc981fb133294b
Victor Guerra <victor.eg@aluno.ifsc.edu.br>
2023-08-09 16:16:33 -0300

    Parte Quatro 2.0

--- lista-01/main.c
+++ lista-01/main.c
@@ -6,8 +6,7 @@ int main(int argc, char *argv[])
 {
     int escolha, num_1, maior = 0,num_2, sorteado;
     int i,num_3, par = 0, impar = 0, positivo = 0, negativo = 0;
-    int num_4;
-
+    int palpite_do_jogador,num_palpitesm, palpites_possiveis[100] = {0};
 
     srand(time(NULL));
 
@@ -62,15 +61,26 @@ int main(int argc, char *argv[])
             break;
         case 4:
             sorteado = 1 + rand() %100;
-            printf("Escolha um número de 1 a 100:");
-            for(;num_4 != sorteado;){
-                scanf("%d",&num_4);
-                if(sorteado != num_4){
-                    printf("Tente novamente!\n");
-                } else{
-                    printf("Parabéns, você acertou!\n");
+            printf("Advinhe um número de 1 a 100\n");
+            for(;palpite_do_jogador != sorteado;){
+                printf("Escolha um número: ");
+                scanf("%d",&palpite_do_jogador);
+
+                if(palpite_do_jogador < 1 || palpite_do_jogador > 100){
+                    printf("Escolha um número de 1 a 100\n");
                 }
 
+                if(palpites_possiveis[palpite_do_jogador - 1] != 0){
+                    printf("Você já escolheu %d vez(es) o número %d. Escolha outra número.\n", palpites_possiveis[palpite_do_jogador - 1], palpite_do_jogador);
+                }
+
+                palpites_possiveis[palpite_do_jogador - 1]++;
+
+                if(palpite_do_jogador == sorteado){
+                    printf("Parabéns, você acertou!\n");
+                } else{
+                    printf("Tente novamente!\n");
+                }
             }
             break;
         case 5: printf("Não implementado\n"); break;
