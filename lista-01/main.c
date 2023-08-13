d86ebe66f3a17ebfa63032eda4012cc846fe1b92
Victor Guerra <victor.eg@aluno.ifsc.edu.br>
2023-08-13 17:14:49 -0300

    Parte Oito

--- lista-01/main.c
+++ lista-01/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[])
 {
@@ -10,7 +11,8 @@ int main(int argc, char *argv[])
     int palpite_do_jogador,num_palpitesm, palpites_possiveis[100] = {0};
     int tamanho_vetor,vetor[100],matriz[10][10] = {0},faltante = 0;
     char palavra[100],inverso[100];
-
+    char frase[1000],minusculo;
+    int vogais[5] = {0};
 
     srand(time(NULL));
 
@@ -129,7 +131,36 @@ int main(int argc, char *argv[])
                 printf("'%s' não é uma palavra palindroma.\n",palavra);
             }
             break;
-        case 8: printf("Não implementado\n"); break;
+        case 8:
+            printf("Digite sua frase:");
+            scanf(" %[^\n]s",frase);
+            for(i = 0;i < strlen(frase);i++){
+                minusculo = tolower(frase[i]);
+                switch(minusculo){
+                    case 'a':
+                        vogais[0]++;
+                        break;
+                    case 'e':
+                        vogais[1]++;
+                        break;
+                    case 'i':
+                        vogais[2]++;
+                        break;
+                    case 'o':
+                        vogais[3]++;
+                        break;
+                    case 'u':
+                        vogais[4]++;
+                        break;
+                }
+            }
+            printf("A vogal \"A\" aparece %d vezes\n",vogais[0]);
+            printf("A vogal \"E\" aparece %d vezes\n",vogais[1]);
+            printf("A vogal \"I\" aparece %d vezes\n",vogais[2]);
+            printf("A vogal \"O\" aparece %d vezes\n",vogais[3]);
+            printf("A vogal \"U\" aparece %d vezes\n",vogais[4]);
+
+            break;
     }
 
     return 0;
