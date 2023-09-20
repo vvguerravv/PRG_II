330d93bb3212696c0921367b3737a48e4b3810c9
VIctor <victor.eg@aluno.ifsc.edu.br>
2023-09-19 22:12:05 -0300

    Parte 1

--- appprg/main.c
+++ appprg/main.c
@@ -1,22 +1,77 @@
 #include <stdio.h>
 #include <libprg/libprg.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    int escolha;
     int *vetor, tamanho,total,elemento,inicio,fim;
-    char op
+    char op;
 
-    printf("Tamanho do vetor:");
-    scanf("%d",tamanho);
-    criar(vetor,tamanho);
-    povoar(vetor,tamanho);
-    insere_nao_ord(vetor,total,tamanho,elemento);
-    insere_ord(vetor,total,tamanho,elemento);
-    busca_linear(vetor,total,tamanho,elemento);
-    busca_bin_int(vetor,total,elemento);
-    busca_bin_rec(vetor,inicio,fim,elemento);
-    remove_num_ord(vetor,total,tamanho,elemento);
-    remove_num_nao_ord(vetor,total,tamanho,elemento);
+    printf("Escolha o que você quer fazer!\n");
+    printf("1 - Criar uma lista povoada de tamanho n\n");
+    printf("2 - Inserir um novo número\n");
+    printf("3 - Remover um número\n");
+    printf("4 - Buscar por um número usando busca linear\n");
+    printf("5 - Buscar por um número usando busca binária iterativa\n");
+    printf("6 - Buscar por um número usando busca binária recursiva\n");
+    printf("OBS: Selecione 0 para sair\n");
+
+    while(escolha != 0){
+        switch (escolha) {
+            case 1:
+                printf("Selecione o tamanho do vetor: ");
+                scanf("%d",&tamanho);
+                printf("Ordenada ou não ordenada? OBS: Selecione \"O\" para ordenada e \"N\ para não ordenada: ");
+                printf("%c",&op)
+                if(strcmp(op,"O") == 0){
+                    criar(vetor,tamanho);povoar_ord(vetor,total,tamanho);
+                } if(strcmp(op,"N") == 0){
+                criar(vetor,tamanho); povoar_nao_ord(vetor,total,tamanho);
+                }
+                break;
+
+            case 2:
+                printf("Selecione qual número será inserido: ");
+                scanf("%d",&elemento);
+
+                if(strcmp(op,"O") == 0){
+                    insere_ord(vetor,total,tamanho,elemento);
+                } if(strcmp(op,"N") == 0){
+                    insere_nao_ord(vetor,total,tamanho,elemento);
+                }
+                break;
+
+            case 3:
+                printf("Selecione o número que será removido: ");
+                scanf("%d",&elemento);
+
+                if(strcmp(op,"O") == 0){
+                    remove_num_ord(vetor,total,tamanho,elemento);
+                } if(strcmp(op,"N") == 0){
+                    remove_num_nao_ord(vetor,total,tamanho,elemento);
+        }
+                break;
+
+            case 4:
+                printf("Selecione o número que será buscado: ");
+                scanf("%d",&elemento);
+                busca_linear(vetor,total,tamanho,elemento);
+                break;
+
+            case 5:
+                printf("Selecione o número que será buscado: ");
+                scanf("%d",&elemento);
+                busca_bin_int(vetor,total,elemento);
+                break;
+
+            case 6:
+                printf("Selecione o número que será buscado: ");
+                scanf("%d",&elemento);
+                busca_bin_rec(vetor,inicio,fim,elemento);
+                break;
+        }
+    }
+
     libera_memoria(vetor,tamanho);
 
     return 0;
