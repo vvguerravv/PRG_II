ce4c31d50bf8adef29e88261f170ed2247644e50
Victor Guerra <victor.eg@aluno.ifsc.edu.br>
2023-09-25 11:28:00 -0300

    Pilha

--- appprg/main.c
+++ appprg/main.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 #include <libprg/libprg.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-    int escolha;
+    int escolha = -1;
     int *vetor, tamanho,total,elemento,inicio,fim;
-    char op;
+    char op[2] = {};
 
     printf("Escolha o que você quer fazer!\n");
-    printf("1 - Criar uma lista povoada de tamanho n\n");
-    printf("2 - Inserir um novo número\n");
-    printf("3 - Remover um número\n");
-    printf("4 - Buscar por um número usando busca linear\n");
-    printf("5 - Buscar por um número usando busca binária iterativa\n");
-    printf("6 - Buscar por um número usando busca binária recursiva\n");
-    printf("OBS: Selecione 0 para sair\n");
+
 
     while(escolha != 0){
+        printf("1 - Criar uma lista povoada de tamanho n\n");
+        printf("2 - Inserir um novo número\n");
+        printf("3 - Remover um número\n");
+        printf("4 - Buscar por um número usando busca linear\n");
+        printf("5 - Buscar por um número usando busca binária iterativa\n");
+        printf("6 - Buscar por um número usando busca binária recursiva\n");
+        printf("Selecione 0 para sair\n");
+
         switch (escolha) {
             case 1:
                 printf("Selecione o tamanho do vetor: ");
                 scanf("%d",&tamanho);
-                printf("Ordenada ou não ordenada? OBS: Selecione \"O\" para ordenada e \"N\ para não ordenada: ");
-                printf("%c",&op)
+                printf("Ordenada ou não ordenada? OBS: Selecione \"O\" para ordenada e \"N\" para não ordenada: ");
+                printf("%c",&op);
                 if(strcmp(op,"O") == 0){
                     criar(vetor,tamanho);povoar_ord(vetor,total,tamanho);
                 } if(strcmp(op,"N") == 0){
