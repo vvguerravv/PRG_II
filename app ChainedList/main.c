e7def0300711799ceac8a87dadc8a9fc7e765778
VIctor <victor.eg@launo.ifsc.edu.br>
2023-11-02 14:03:43 -0300

    App Lista Encadeada

--- app ChainedList/main.c
+++ app ChainedList/main.c
@@ -1,3 +1,55 @@
+#include <libprg/libprg.h>
+
+int main()
+{
+    no_t *no;
+    ChainedList_t list;
+    list.size = 0;
+    Stackist_t stack;
+    stack.size = 0;
+
+
+    addStack(&stack,&no,10);
+    addStack(&stack,&no,20);
+    addStack(&stack,&no,110);
+    addStack(&stack,&no,1110);
+
+    printNo(no);
+    printf("\n");
+    printf("Posição do 10: %d\n", searchStack(&no,10));
+
+    printf("\n Retira 1: ");
+    removeStack(&stack,&no);
+    printNo(no);
+    printf("\n");
+    printf("Posição do 10: %d\n", searchStack(&no,10));
+
+    printf("\n Retira 2: ");
+    removeStack(&stack,&no);
+    printNo(no);
+    printf("\n");
+    printf("Posição do 10: %d\n", searchStack(&no,10));
+
+    printf("\n Retira 3: ");
+    removeStack(&stack,&no);
+    printNo(no);
+    printf("\n");
+    printf("Posição do 10: %d\n", searchStack(&no,10));
+//    addList(&list,&no,13);
+//    addList(&list,&no,35);
+//    addList(&list,&no,80);
 //
-// Created by guerrra on 02/11/23.
+//    printf("Nós: ");
+//    printNo(no);
+//    printf("Tamanho: %d\n",list.size);
 //
+//    removeList(&list,&no);
+//
+//    printf("Nós: ");
+//    printNo(no);
+//    printf("Tamanho: %d\n",list.size);
+//
+//    printf("%d",searchList(&no,32));
+
+    return 0;
+}
